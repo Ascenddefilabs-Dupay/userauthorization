7e8b7faf05a051e0c793b8af9d6e1a26705884ab
sowmyanaidu1117 <Lakshmi.Sowmya@ascenddefi.com>
2024-10-10 16:44:54 +0530

    deleted password backend

--- backend-userauthorization/userauthorization/models.py
+++ backend-userauthorization/userauthorization/models.py
@@ -33,50 +33,6 @@ class CustomUser(models.Model):
         db_table = 'users'
         managed = False
 
-# class Notificationthings(models.Model):
-#     user_id = models.CharField(max_length=255, unique=True, primary_key=True, default='dupA0005')
-#     product_announcement = models.BooleanField(default=True)
-#     insights_tips = models.BooleanField(default=True)
-#     special_offers = models.BooleanField(default=True)
-#     price_alerts = models.BooleanField(default=True)
-#     account_activity = models.BooleanField(default=True)
-#     messages = models.BooleanField(default=True)
-
-#     class Meta:
-#         db_table = 'notification_settings'
-#         managed = False
-
-
-# class Password(models.Model):
-#     password_creation = models.CharField(max_length=6)
-#     retype_password = models.CharField(max_length=6)
-
-#     class Meta:
-#         db_table = 'app_password'
-#         managed = False
-
-class Notificationthings(models.Model):
-    user_id = models.CharField(max_length=255, unique=True, primary_key=True)
-    product_announcement = models.BooleanField(default=True)
-    insights_tips = models.BooleanField(default=True)
-    special_offers = models.BooleanField(default=True)
-    price_alerts = models.BooleanField(default=True)
-    account_activity = models.BooleanField(default=True)
-    messages = models.BooleanField(default=True)
-
-    class Meta:
-        db_table = 'notification_settings'
-        managed = False
-
-
-class Password(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
-    password_creation = models.CharField(max_length=255)
-    retype_password = models.CharField(max_length=225)
-
-    class Meta:
-        db_table = 'app_password'
-        managed = False
         
 class FiatWallet(models.Model):
     fiat_wallet_id = models.CharField(
